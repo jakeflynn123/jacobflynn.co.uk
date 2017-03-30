95f84e6407f1c4e0779e65c46be960d1a107c667
Jacob Flynn <jacobflynn@jacobs-mbp.home>
2017-03-30 11:31:44 +0100

    made it mobile responsive

--- scripts/navbarChange.js
+++ scripts/navbarChange.js
@@ -1,32 +1,55 @@
 /**
  * Created by Jacob Flynn on 02/03/2017, all content and code copyright Jacob Flynn © 2017
  */
-
-$(document).ready(function(){
-    $(window).scroll(function() { // check if scroll event happened
-        if ($(document).scrollTop() > 20) { // check if user scrolled more than 20 from top of the browser window
-            $("nav > ul").addClass("scrolled");
-            $("nav > ul > li").addClass("scrolled");
-            $("nav > ul > li:nth-child(2)").fadeIn();
-            $("nav > ul > li:nth-child(3)").fadeIn();
-            $("nav > ul > li:nth-child(4)").fadeIn();
-            $("nav > ul > li:nth-child(5)").fadeIn();
-            $("nav > ul > li:first-child").css("font-size", "20px");
-
-        } else {
-            $("nav > ul").removeClass("scrolled");
-            $("nav > ul > li").removeClass("scrolled");
-            $("nav > ul > li:nth-child(2)").fadeOut();
-            $("nav > ul > li:nth-child(3)").fadeOut();
-            $("nav > ul > li:nth-child(4)").fadeOut();
-            $("nav > ul > li:nth-child(5)").fadeOut();
-            $("nav > ul > li:first-child").css("font-size", "30px");
-
-        }
-    });
+$(document).ready(function () {
     $('#downarrow').click(function () {
         $("body, html").animate({
             scrollTop: $("#jumper").offset().top
         }, 600);
+    });
+});
+
+var mq = window.matchMedia( "(min-width: 805px)" );
+if (mq.matches) {
+    $(document).ready(function () {
+        $(window).scroll(function () { // check if scroll event happened
+            if ($(document).scrollTop() > 20) { // check if user scrolled more than 20 from top of the browser window
+                $("nav > ul").addClass("scrolled");
+                $("nav > ul > li").addClass("scrolled");
+                $("nav > ul > li:nth-child(2)").fadeIn();
+                $("nav > ul > li:nth-child(3)").fadeIn();
+                $("nav > ul > li:nth-child(4)").fadeIn();
+                $("nav > ul > li:nth-child(5)").fadeIn();
+                $("nav > ul > li:first-child").css("font-size", "20px");
+
+            } else {
+                $("nav > ul").removeClass("scrolled");
+                $("nav > ul > li").removeClass("scrolled");
+                $("nav > ul > li:nth-child(2)").fadeOut();
+                $("nav > ul > li:nth-child(3)").fadeOut();
+                $("nav > ul > li:nth-child(4)").fadeOut();
+                $("nav > ul > li:nth-child(5)").fadeOut();
+                $("nav > ul > li:first-child").css("font-size", "30px");
+
+            }
+        });
+    });
+}
+$(document).ready(function(){
+    $('#nav-icon4').click(function(){
+        if ($('#menu').css('display') === 'none') {
+            $(this).toggleClass('open');
+            $('main').css('display', 'none');
+            $('#menu').slideDown();
+            $(this).css('border-color', 'white');
+            $(this).css('background-color', 'white');
+        }else{
+            $(this).toggleClass('open');
+            $('main').css('display', 'block');
+            $('#menu').slideUp();
+            $(this).css('border-color', 'black');
+            $(this).css('background-color', 'black');
+            $("#nav-icon4 > span").css('background-color', '#0072CA');
+        }
     });
 });
--- scripts/skills.js
+++ scripts/skills.js
@@ -1,7 +1,50 @@
 /**
  * Created by Jacob Flynn on 02/03/2017, all content and code copyright Jacob Flynn © 2017
  */
+
 $(function () {
+    function myFunction(x) {
+        x.classList.toggle("change");
+    }
+    $('.wrapper > div:nth-child(1) > img').mouseenter(function () {
+        $('.wrapper > div:nth-child(1) > .skillstext').fadeIn(100,function(){
+            $('.skill').mouseleave(function(){
+                $('.skillstext').fadeOut(100);
+            });
+        });
+    });
+    $('.wrapper > div:nth-child(2) > img').mouseenter(function () {
+        $('.wrapper > div:nth-child(2) > .skillstext').fadeIn(100,function(){
+            $('.skill').mouseleave(function(){
+                $('.skillstext').fadeOut(100);
+            });
+        });
+    });
+    $('.wrapper > div:nth-child(3) > img').mouseenter(function () {
+        $('.wrapper > div:nth-child(3) > .skillstext').fadeIn(100,function(){
+            $('.skill').mouseleave(function(){
+                $('.skillstext').fadeOut(100);
+            });
+        });
+    });
+    $('.wrapper > div:nth-child(4) > img').mouseenter(function () {
+        $('.wrapper > div:nth-child(4) > .skillstext').fadeIn(100,function(){
+            $('.skill').mouseleave(function(){
+                $('.skillstext').fadeOut(100);
+            });
+        });
+    });
+    $('.wrapper > div:nth-child(5) > img').mouseenter(function () {
+        $('.wrapper > div:nth-child(5) > .skillstext').fadeIn(100,function(){
+            $('.skill').mouseleave(function(){
+                $('.skillstext').fadeOut(100);
+            });
+        });
+    });
+
+
+
+
     $('#showall').click(function () {
         $(this).fadeIn("slow", function () {
             $(this).addClass('clicked');
